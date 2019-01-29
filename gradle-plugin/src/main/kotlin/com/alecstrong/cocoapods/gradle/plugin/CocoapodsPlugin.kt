178a1c8c114f809154e0f1dc1c4b1b66438ce013
Alec Strong <astrong@squareup.com>
2019-01-29 08:30:51 -0500

    Modify the syntax for specifying the cocoapods target architectures

--- gradle-plugin/src/main/kotlin/com/alecstrong/cocoapods/gradle/plugin/CocoapodsPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/alecstrong/cocoapods/gradle/plugin/CocoapodsPlugin.kt
@@ -1,10 +1,10 @@
 package com.alecstrong.cocoapods.gradle.plugin
 
 import groovy.lang.Closure
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinTargetPreset
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeOutputKind
 
@@ -13,40 +13,9 @@ open class CocoapodsPlugin : Plugin<Project> {
     val extension = project.extensions.create("cocoapods", CocoapodsExtension::class.java)
     val mppExtension = project.extensions.getByType(KotlinMultiplatformExtension::class.java)
 
-    var architectures: List<String>? = null
-
-    project.extensions.add("targetForCocoapods", object : Closure<Unit>(extension) {
-      override fun call(vararg args: Any) {
-        architectures = extension.architectures
-        val name = args[0] as? String
-            ?: throw IllegalArgumentException("Expected string for first argument to targetForCocoapods")
-        val closure: Closure<*>? = when {
-          args.size == 2 -> args[1] as? Closure<*>
-              ?: throw IllegalArgumentException("Expected closure for second argument to targetForCocoapods")
-          args.size > 2 -> throw IllegalArgumentException("Expected two arguments to targetForCocoapods")
-          else -> null
-        }
-        val preset = CocoapodsTargetPreset(project, closure)
-        mppExtension.targetFromPreset(preset, name)
-      }
-    })
+    configureTarget(project)
 
     project.afterEvaluate {
-      if (architectures != null && (extension.architectures != architectures)) {
-        throw GradleException("""
-          |If specifying architectures in the cocoapods configuration, is must be above the kotlin
-          |configuration:
-          |
-          |cocoapods {
-          |  architectures = ${extension.architectures}
-          |}
-          |
-          |kotlin {
-          |  ...
-          |}
-        """.trimMargin())
-      }
-
       project.tasks.register("generatePodspec", GeneratePodspecTask::class.java) { task ->
         task.group = GROUP
         task.description = "Generate a podspec file for this Kotlin Native project"
@@ -62,7 +31,8 @@ open class CocoapodsPlugin : Plugin<Project> {
 
       project.tasks.register("initializeFramework", InitializeFrameworkTask::class.java) { task ->
         task.group = GROUP
-        task.description = "Create a dummy dynamic framework to be used during Cocoapods installation"
+        task.description =
+          "Create a dummy dynamic framework to be used during Cocoapods installation"
       }
 
       mppExtension.targets
@@ -72,8 +42,10 @@ open class CocoapodsPlugin : Plugin<Project> {
           .map { it.buildType to it.linkTask }
           .groupBy({ it.first }, { it.second })
           .forEach { (buildType, compilations) ->
-            project.tasks.register("createIos${buildType.name()}Artifacts",
-                CocoapodsCompileTask::class.java) { task ->
+            project.tasks.register(
+                "createIos${buildType.name()}Artifacts",
+                CocoapodsCompileTask::class.java
+            ) { task ->
               task.dependsOn(compilations)
               task.buildType = buildType
               task.compilations = compilations
@@ -83,7 +55,71 @@ open class CocoapodsPlugin : Plugin<Project> {
     }
   }
 
+  private fun configureTarget(project: Project) {
+    val extension = project.extensions.getByType(CocoapodsExtension::class.java)
+    val mppExtension = project.extensions.getByType(KotlinMultiplatformExtension::class.java)
+
+    project.extensions.add("targetForCocoapods", object : Closure<Unit>(extension) {
+      override fun call(vararg args: Any) {
+        var presets = listOf(
+            mppExtension.presets.getByName("iosArm64"),
+            mppExtension.presets.getByName("iosArm32"),
+            mppExtension.presets.getByName("iosX64")
+        )
+
+        val name: String
+        val closure: Closure<*>?
+
+        fun closureForArg(index: Int): Closure<*>? {
+          return when {
+            args.size == index + 1 -> args[index] as? Closure<*>
+              ?: throw IllegalArgumentException(
+                  "Expected closure for argument at index $index to targetForCocoapods"
+              )
+
+            args.size > index + 1 -> throw IllegalArgumentException(
+                "Expected ${index + 1} arguments to targetForCocoapods"
+            )
+
+            else -> null
+          }
+        }
+
+        when (val argument = args[0]) {
+          is String -> {
+            name = argument
+            closure = closureForArg(1)
+          }
+
+          is List<*> -> {
+            presets = argument
+                .map {
+                  if (it !is KotlinTargetPreset<*>) {
+                    throw IllegalStateException(
+                        "Expected list of presets (example: [presets.iosArm64, presets.iosX64])"
+                    )
+                  }
+                  return@map (it as KotlinTargetPreset<*>)
+                }
+                .ifEmpty {
+                  throw IllegalArgumentException(
+                      "Expected list of presets for first argument to targetForCocoapods"
+                  )
+                }
+            name = args[1] as? String
+                ?: throw IllegalArgumentException("Expected string for first argument to targetForCocoapods")
+            closure = closureForArg(2)
+          }
+          else -> throw IllegalStateException("Expected a list of presets or a string name for the first argument to targetForCocoapods")
+        }
+
+        val preset = CocoapodsTargetPreset(project, closure, presets)
+        mppExtension.targetFromPreset(preset, name)
+      }
+    })
+  }
+
   companion object {
     internal const val GROUP = "cocoapods"
- }
+  }
 }
